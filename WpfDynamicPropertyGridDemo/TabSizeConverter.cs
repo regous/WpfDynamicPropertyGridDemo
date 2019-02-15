6b990a278724696699e9855c1bd2906fd31b0756
Ous <regous@163.com>
2019-02-16 00:05:09 +0800

    美化界面

---
+++ WpfDynamicPropertyGridDemo/TabSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfDynamicPropertyGridDemo
+{
+    public class TabSizeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            System.Windows.Controls.TabControl tabControl = values[0] as System.Windows.Controls.TabControl;
+            double width = tabControl.ActualWidth / tabControl.Items.Count;
+            //Subtract 1, otherwise we could overflow to two rows.
+            return (width <= 1) ? 0 : (width - 1);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ WpfDynamicPropertyGridDemo/UniformTabPanel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace WpfDynamicPropertyGridDemo
+{
+    public class UniformTabPanel : UniformGrid
+    {
+        public UniformTabPanel()
+        {
+            this.IsItemsHost = true;
+            this.Rows = 1;
+
+            //Default, so not really needed..
+            this.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var children = this.Children.OfType<TabItem>();
+            var totalMaxWidth = children.Sum(tab => tab.MaxWidth);
+            if (!double.IsInfinity(totalMaxWidth))
+            {
+                this.HorizontalAlignment = (constraint.Width > totalMaxWidth)
+                                                    ? HorizontalAlignment.Left
+                                                    : HorizontalAlignment.Stretch;
+                foreach (var child in children)
+                {
+                    child.Width = this.HorizontalAlignment == System.Windows.HorizontalAlignment.Left
+                            ? child.MaxWidth
+                            : Double.NaN;
+                }
+            }
+            return base.MeasureOverride(constraint);
+        }
+    }
+}
